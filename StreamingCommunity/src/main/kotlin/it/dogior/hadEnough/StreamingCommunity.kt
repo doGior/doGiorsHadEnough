c937f4958c76a7e5756c7674868f3d051605112d
Michele Golino <34338028+micheleGolino@users.noreply.github.com>
2026-02-18 13:45:14 +0100

    Update mainUrl to new Streaming Community URL
    The new StreamingUnity website now requires user registration to access the content.
    StreamingCommunityZ, instead, allows users to watch videos without registration, so no changes to the current code flow are required.

--- StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
+++ StreamingCommunity/src/main/kotlin/it/dogior/hadEnough/StreamingCommunity.kt
@@ -48,7 +48,7 @@ class StreamingCommunity(override var lang: String = "it") : MainAPI() {
             "X-Inertia-Version" to inertiaVersion,
             "X-Requested-With" to "XMLHttpRequest",
         ).toMutableMap()
-        val mainUrl = "https://streamingunity.club/"
+        val mainUrl = "https://streamingcommunityz.motorcycles/"
         var name = "StreamingCommunity"
         val TAG = "SCommunity"
     }
