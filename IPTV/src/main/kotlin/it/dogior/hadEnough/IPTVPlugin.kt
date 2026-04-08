57d744a09fffd12e0d6f927a61cddfdf0f37d703
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-08 15:06:33 -0600

    Migrate AcraApplication to CloudStreamApp (#87)

--- IPTV/src/main/kotlin/it/dogior/hadEnough/IPTVPlugin.kt
+++ IPTV/src/main/kotlin/it/dogior/hadEnough/IPTVPlugin.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.plugins.*
-import com.lagradost.cloudstream3.AcraApplication.Companion.getKey
+import com.lagradost.cloudstream3.CloudStreamApp.Companion.getKey
 
 @CloudstreamPlugin
 class IPTVPlugin : Plugin() {
