0721843178a18da4286c2192ef705318f4a22f39
doGior <43815981+doGior@users.noreply.github.com>
2026-08-23 22:51:10 +0200

    It works 😀

--- CalcioStreaming/src/main/kotlin/it/dogior/hadEnough/CalcioStreaming.kt
+++ CalcioStreaming/src/main/kotlin/it/dogior/hadEnough/CalcioStreaming.kt
@@ -1,77 +1,68 @@
 package it.dogior.hadEnough
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.network.CloudflareKiller
 import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import okhttp3.Interceptor
 import okhttp3.Response
 import org.json.JSONObject
 import kotlin.io.encoding.Base64
 
+
 class CalcioStreaming : MainAPI() {
     override var lang = "it"
-    override var mainUrl = "https://fig.direttecommunity.online/"
+    override var mainUrl = "https://corner.direttecommunity.online/"
     override var name = "CalcioStreaming"
     override val hasMainPage = true
     override val hasChromecastSupport = true
     override val supportedTypes = setOf(TvType.Live)
     override var sequentialMainPage = true
+    override val hasDownloadSupport = false
     val cfKiller = CloudflareKiller()
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get(
-            "$mainUrl/partite-streaming.html"
-        ).document
-        val sections =
-            document.select("div.slider-title")
-                .filter { it -> it.select("div.owl-carousel").isNotEmpty() }
-        if (sections.isEmpty()) throw ErrorLoadingException()
-
-        return newHomePageResponse(sections.mapNotNull { it ->
-            val categoryName = it.selectFirst("div.header-wrap > div.label")!!.text()
-            val shows = it.select("div.owl-carousel > .slider-tile-inner > .box-16x9").map {
-                val href = it.selectFirst("a")!!.attr("href")
-                val name = it.selectFirst(".tile-title")?.text() ?: ""
-                val posterUrl = fixUrl(it.selectFirst("img.tile-image")!!.attr("src"))
-                    .replace("//uploads", "/uploads")
-                newLiveSearchResponse(name, href, TvType.Live) {
-                    this.posterUrl = posterUrl
-                }
+        val resp = app.get("$mainUrl/api/events.php").body.string()
+        val events = parseJson<JSONResponse>(resp).events
+        val searchResponses = events.mapNotNull { event ->
+            if (event.status == "live") {
+                newLiveSearchResponse(
+                    name = event.title,
+                    url = event.toJson()
+                )
+            } else {
+                null
             }
-            if (shows.isEmpty()) return@mapNotNull null
-            HomePageList(
-                categoryName,
-                shows,
-                isHorizontalImages = true
-            )
-        }, false)
+        }
 
+        return newHomePageResponse(
+            listOf(
+                HomePageList(
+                    "Live", searchResponses, true
+                )
+            ), false
+        )
     }
 
 
     override suspend fun load(url: String): LoadResponse {
-        val document = app.get(url).document
-        val posterUrl =
-            document.select("div.background-image.bg-image").attr("style").substringAfter("url(")
-                .substringBefore(");")
-        val infoBlock = document.select(".info-wrap")
-//        val title = infoBlock.select("h1").text()
-        val title = document.selectFirst("meta[property=\"og:title\"]")?.attr("content") ?: ""
-        val description = infoBlock.select("div.info-span > span").toList().joinToString(" - ")
-        return newLiveStreamLoadResponse(name = title, url = url, dataUrl = url) {
-            this.posterUrl = fixUrl(posterUrl)
-                .replace("//uploads", "/uploads")
-            this.plot = description
-        }
+        val data = url
+        val event = parseJson<Event>(data)
+//        val posterUrl = ""
+        val title = event.title
+        return newLiveStreamLoadResponse(title, url = url, dataUrl = event.streams.toJson())
     }
 
     fun getStreamUrl(html: String): String? {
         val configMatch = Regex("""window\._econfig\s*=\s*['"]([^'"]+)['"]""").find(html)
             ?: return null
 
-        return try {
+        try {
             val encodedConfig = configMatch.groupValues[1]
             val decodedConfig =
                 Base64.decode(encodedConfig + "=".repeat((-encodedConfig.length % 4 + 4) % 4))
@@ -104,16 +95,15 @@ class CalcioStreaming : MainAPI() {
                 .toString(Charsets.UTF_8)
 
             val config = JSONObject(configJson)
-            config.optString("stream_url_nop2p").ifEmpty { null }
+            return config.optString("stream_url_nop2p").ifEmpty { null }
                 ?: config.optString("stream_url").ifEmpty { null }
         } catch (_: Exception) {
-            null
+            return null
         }
     }
 
     private suspend fun extractVideoStream(
         url: String,
-        ref: String,
         n: Int
     ): Pair<String, String>? {
         if (url.toHttpUrlOrNull() == null) return null
@@ -121,19 +111,20 @@ class CalcioStreaming : MainAPI() {
 
         val doc = app.get(url).document
         val link = doc.selectFirst("iframe")?.attr("src") ?: return null
-        val newPage = app.get(
-            fixUrl(link), referer = ref, headers = mapOf(
+        val referer = "https://"+url.toHttpUrl().host
+        val resp2 = app.get(
+            fixUrl(link), referer = referer, headers = mapOf(
                 "Sec-Fetch-Dest" to "iframe"
             )
-        ).document
+        )
+        val newPage = resp2.document
         val streamUrl = getStreamUrl(newPage.toString())
         return if (newPage.select("script").size >= 6 && !streamUrl.isNullOrEmpty()) {
             streamUrl to fixUrl(link)
         } else {
-            extractVideoStream(url = link, ref = url, n = n + 1)
+            extractVideoStream(url = link, n = n + 1)
         }
     }
-
 
     override suspend fun loadLinks(
         data: String,
@@ -141,24 +132,26 @@ class CalcioStreaming : MainAPI() {
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
-        val document = app.get(data).document
-        val links = document.select("div.embed-container > div.langs > button").mapNotNull {
-            val lang = it.text()
-            val url = it.attr("data-link")
-            val link = extractVideoStream(url, url.substringBefore("channels"), 1)
-            if (link == null) return@mapNotNull null
-            Link(lang, link.first, link.second)
+        val streams = parseJson<List<Stream>>(data)
+        val links = streams.mapNotNull { stream ->
+            Log.d("CalcioStreaming", stream.toJson())
+            val link = extractVideoStream(url=stream.url,n=0)
+            Log.d("CalcioStreaming", "Link finale: $link\n")
+            if(link != null){
+                Link(name="${stream.label} ${stream.lang}", ref=link.second, url = link.first)
+            }else{
+                null
+            }
         }
-        links.map {
-            Log.d("CalcioStreaming", it.toString())
+        links.forEach {
+            Log.d("CalcioStreaming", it.toJson())
             callback(
                 newExtractorLink(
                     source = this.name,
-                    name = it.lang,
+                    name = it.name,
                     url = it.url,
                     type = ExtractorLinkType.M3U8
                 ) {
-                    this.quality = 0
                     this.referer = it.ref
                 }
             )
@@ -174,10 +167,56 @@ class CalcioStreaming : MainAPI() {
             }
         }
     }
-
-    data class Link(
-        val lang: String,
-        val url: String,
-        val ref: String
-    )
 }
+
+data class Link(
+    val name: String,
+    val url: String,
+    val ref: String
+)
+data class JSONResponse(
+    @JsonProperty("events")
+    val events: List<Event>
+)
+
+data class Event(
+//    @JsonProperty("away_team")
+//    val awayTeam: String,
+//    @JsonProperty("away_team_badge")
+//    val awayTeamBadge: String,
+//    @JsonProperty("heat_tier")
+//    val heatTier: String,
+//    @JsonProperty("home_team")
+//    val homeTeam: String,
+//    @JsonProperty("home_team_badge")
+//    val homeTeamBadge: String,
+    @JsonProperty("id")
+    val id: String,
+//    @JsonProperty("league")
+//    val league: String,
+//    @JsonProperty("sources")
+//    val sources: List<String>,
+    @JsonProperty("sport")
+    val sport: String,
+    @JsonProperty("start_time")
+    val startTime: String,
+    @JsonProperty("start_ts")
+    val startTs: Int,
+    @JsonProperty("status")
+    val status: String,
+    @JsonProperty("streams")
+    val streams: List<Stream>,
+    @JsonProperty("title")
+    val title: String
+)
+
+data class Stream(
+    @JsonProperty("label")
+    val label: String,
+    @JsonProperty("lang")
+    val lang: String,
+    @JsonProperty("source")
+    val source: String,
+    @JsonProperty("url")
+    val url: String
+)
