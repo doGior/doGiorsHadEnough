7407f56eacfef198b1378e640cef5d2852239af4
doGior <43815981+doGior@users.noreply.github.com>
2026-09-16 19:20:21 +0200

    Update CalcioStreaming.kt

--- CalcioStreaming/src/main/kotlin/it/dogior/hadEnough/CalcioStreaming.kt
+++ CalcioStreaming/src/main/kotlin/it/dogior/hadEnough/CalcioStreaming.kt
@@ -18,7 +18,7 @@ import kotlin.io.encoding.Base64
 
 class CalcioStreaming : MainAPI() {
     override var lang = "it"
-    override var mainUrl = "https://angolo.direttecommunity.online/"
+    override var mainUrl = "https://finale.direttecommunity.online/"
     override var name = "CalcioStreaming"
     override val hasMainPage = true
     override val hasChromecastSupport = true
